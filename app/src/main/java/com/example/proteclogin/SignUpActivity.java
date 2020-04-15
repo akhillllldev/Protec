627dc386cdac384f4464fa2d396b6e8ff9501064
akhillllldev <akhil.surendran@vitap.ac.in>
2020-04-15 17:18:10 +0530

    first commit

---
+++ app/src/main/java/com/example/proteclogin/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.example.proteclogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void OpenLoginPage(View view){
+        startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+    }
+}
